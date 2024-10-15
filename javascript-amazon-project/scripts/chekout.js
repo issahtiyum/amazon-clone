2998631ee52bf0bd2153084e5d7cb82a9144dfd6
harisissah <myharis.issah@gmail.com>
2024-10-15 22:27:24 +0000

    Delete products from HTML when clicking delete

--- javascript-amazon-project/scripts/chekout.js
+++ javascript-amazon-project/scripts/chekout.js
@@ -13,7 +13,7 @@ cart.forEach((cartItem) => {
     }
   })
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Wednesday, June 15
       </div>
@@ -97,6 +97,8 @@ document.querySelector('.js-order-summary')
       link.addEventListener('click', ()=> {
         const {productId} = link.dataset;
         removeFromCart(productId);
-        console.log(cart)
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`)
+        container.remove()
       })
     })
