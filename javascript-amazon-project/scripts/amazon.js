54e35b121aa4c8256bc610bbae6eaf9c4049ce9e
harisissah <myharis.issah@gmail.com>
2024-10-15 17:05:51 +0000

    Move addToCart to cart.js

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
 let productsHTML = ''
@@ -55,51 +55,30 @@ products.forEach((product) => {
     </div>
   `
 })
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-document.querySelector('.js-products-grid').innerHTML = productsHTML
+function updateCartQuantity(){
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity
+  })
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
+}
 
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
-
     let addedMessageTimeoutId;
 
     button.addEventListener('click', ()=>{
       const {productId} = button.dataset;
-
-      let matchingItem;
-
-      cart.forEach((item) => {
-        if (productId === item.productId) {
-          matchingItem = item
-        }
-      })
-
-      const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`)
-
-      const quantity = Number(quantitySelector.value);
-
-      if (matchingItem) {
-        matchingItem.quantity += quantity
-      } else {
-          cart.push({
-            productId,
-            quantity
-        });
-      }
-
-      let cartQuantity = 0;
-
-      cart.forEach((item) => {
-        cartQuantity += item.quantity
-      })
-      document.querySelector('.js-cart-quantity')
-        .innerHTML = cartQuantity;
-
+      addToCart(productId);
+      updateCartQuantity()
+    
       document.querySelector(`.added-${productId}`)
         .classList.add('show-added')
 
       if (addedMessageTimeoutId) clearTimeout(addedMessageTimeoutId);
-      
       addedMessageTimeoutId = setTimeout(() => {
         document.querySelector(`.added-${productId}`)
         .classList.remove('show-added')
