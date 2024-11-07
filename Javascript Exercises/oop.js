ff9f673dfe43e5f0f42a0d9b41cb21a4735a85d6
harisissah <myharis.issah@gmail.com>
2024-11-07 23:55:03 +0000

    creating racecar child

--- Javascript Exercises/oop.js
+++ Javascript Exercises/oop.js
@@ -1,20 +1,21 @@
 class Car {
-  brand;
-  model;
+  #brand;
+  #model;
 
   constructor(carDetails) {
-    this.brand = carDetails.brand;
-    this.model = carDetails.model;
+    this.#brand = carDetails.brand;
+    this.#model = carDetails.model;
   }
 
   displayInfo (){
-    console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h, Is the trunk open? ${this.isTrunkOpen}`)
+    console.log(`${this.#brand} ${this.#model}, Speed: ${this.speed} km/h, Is the trunk open? ${this.isTrunkOpen}`)
   }
 
   speed = 0;
+  maxSpeed = 200;
   isTrunkOpen = false;
   go(){ 
-    if(this.speed === 200) {
+    if(this.speed === maxSpeed) {
       console.log('maximum speed reached')
       return;
     }
@@ -48,3 +49,30 @@ const car1 = new Car({brand: 'Tesla', model: 'Model 3'});
 car1.displayInfo();
 car2.displayInfo();
 
+class RaceCar extends Car {
+  acceleration;
+  constructor(carDetails){
+    super(carDetails)
+    this.acceleration = carDetails.acceleration;
+  }
+  maxSpeed = 300;
+  openTrunk(){
+    console.log("racecars don't have trunks!")
+  }
+  closeTrunk() {
+    console.log("racecars don't have trunks!")
+  }
+  go(){
+    if(this.speed === this.maxSpeed) {
+      console.log('maximum speed reached')
+      return;
+    }
+    this.speed += this.acceleration;
+    this.displayInfo()
+  }
+}
+
+const raceCar = new RaceCar({brand: 'McLaren', model: 'F1', acceleration: 20})
+raceCar.openTrunk()
+raceCar.go()
+raceCar.displayInfo()
